4fcfa6f2d67e8310976b198d585d1dd501a0d748
Katherine <katherinepacheco001@gmail.com>
2021-06-29 12:14:34 -0400

    initial commit
    initial commit with set up

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi quia qui, 
+      labore ex possimus veritatis quo, doloribus temporibus, dignissimos aspernatur 
+      soluta enim ut voluptatum nesciunt ipsam fugit rem quisquam neque.
+      </p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi quia qui, 
+      labore ex possimus veritatis quo, doloribus temporibus, dignissimos aspernatur 
+      soluta enim ut voluptatum nesciunt ipsam fugit rem quisquam neque.
+      </p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi quia qui, 
+      labore ex possimus veritatis quo, doloribus temporibus, dignissimos aspernatur 
+      soluta enim ut voluptatum nesciunt ipsam fugit rem quisquam neque.
+      </p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi quia qui, 
+      labore ex possimus veritatis quo, doloribus temporibus, dignissimos aspernatur 
+      soluta enim ut voluptatum nesciunt ipsam fugit rem quisquam neque.
+      </p>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi quia qui, 
+      labore ex possimus veritatis quo, doloribus temporibus, dignissimos aspernatur 
+      soluta enim ut voluptatum nesciunt ipsam fugit rem quisquam neque.
+      </p>
   </div>
 </template>
