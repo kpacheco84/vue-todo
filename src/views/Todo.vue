adbe5b454ea6025f9540dd5ecb9d6ffcfe36cecc
Katherine <katherinepacheco001@gmail.com>
2021-06-29 12:53:07 -0400

    todo added done and delete
    added done and delete functionality

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,44 @@
 <template>
-<div class="home pa-6">
-<h1>Todo Page </h1>
+<div >
+<v-list
+      subheader
+    class="pt-0"
+    >
+ 
+<div
+v-for="task in tasks"
+        :key="task.id"
+        >
+  
+        <v-list-item
+        @click="doneTask(task.id)"
+        :class="{'blue lighten-5': task.done}"
+        >
+          
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done"
+              color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title :class="{'text-decoration-line-through': task.done}">{{task.title}}</v-list-item-title>
+            </v-list-item-content>
+              <v-list-item-action>
+          <v-btn 
+          icon
+          @click.stop="deleteTask(task.id)"
+          >
+            <v-icon color="primary lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+</div>
+      
+   
+    </v-list>
   </div>
 </template>
 
@@ -9,6 +47,24 @@
 
   export default {
     name: 'Todo',
+    data(){
+      return{
+        tasks:[
+          {id:1,title:"Wake up", done: false},
+           {id:2,title:"Code", done: false},
+            {id:3,title:"Coffee", done: false},
+        ]
+      }
+    },
+    methods:{
+      doneTask(id){
+        let task = this.tasks.filter(task=> task.id === id)[0]
+        task.done = !task.done
+      },
+      deleteTask(id){
+        this.tasks = this.tasks.filter(task=> task.id !== id)
+      }
+    }
 
   
   }
