35b9e635fd3f5efec9444a1b938624d3b26a0bfc
Katherine <katherinepacheco001@gmail.com>
2021-06-29 20:22:34 -0400

    task menu added and delete
    added a task menu to each task and hooked up the delete function/method

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -1,61 +1,41 @@
 <template>
   <div>
-  
-        <v-list-item
-        @click="$store.commit('doneTask',task.id)"
-        :class="{'blue lighten-5': task.done}"
-        >
-          
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done"
-              color="primary"></v-checkbox>
-            </v-list-item-action>
-
-            <v-list-item-content>
-              <v-list-item-title :class="{'text-decoration-line-through': task.done}">{{task.title}}</v-list-item-title>
-            </v-list-item-content>
-              <v-list-item-action>
-          <v-btn 
-          icon
-          @click.stop="dialogs.delete = true"
-          >
-            <v-icon color="primary lighten-1">mdi-delete</v-icon>
-          </v-btn>
+    <v-list-item
+      @click="$store.commit('doneTask', task.id)"
+      :class="{ 'blue lighten-5' : task.done }"
+    >
+      <template v-slot:default>
+        <v-list-item-action>
+          <v-checkbox
+            :input-value="task.done"
+            color="primary"
+          ></v-checkbox>
         </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-        <dialog-delete 
-        @close="dialogs.delete= false"
-        v-if="dialogs.delete" 
-        :task="task"
 
-        
-        />
-</div>
-     
+        <v-list-item-content>
+          <v-list-item-title
+            :class="{ 'text-decoration-line-through' : task.done }"
+          >
+            {{ task.title }}
+          </v-list-item-title>
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <task-menu :task="task" />
+        </v-list-item-action>
+      </template>
+
+    </v-list-item>
+    <v-divider></v-divider>
+
+  </div>
 </template>
 
 <script>
-
 export default {
-   
   props: ['task'],
-  data(){
-    return{
-      dialogs:{
-        delete:false
-      }
-    }
-  },
-  components:{
-    'dialog-delete':require('@/components/Todo/Dialogs/DialogDelete.vue').default
-    
+  components: {
+    'task-menu': require('@/components/Todo/TaskMenu.vue').default
   }
 }
 </script>
-
-<style>
-
-</style>
